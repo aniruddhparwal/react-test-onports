957301700b5458a11ac690c698ae4a1b43b3f021
Aniruddh Parwal <aniruddhparwal@gmail.com>
2020-09-08 17:29:08 +0530

    Updates

---
+++ precache-manifest.6d922de74d408c7bde9584b44f9e2e62.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4850f38d072ff35b067672d9d0d97988",
+    "url": "/react-test-onports/index.html"
+  },
+  {
+    "revision": "5cf4e77db0d742c4a0b8",
+    "url": "/react-test-onports/static/css/main.7cb3a40e.chunk.css"
+  },
+  {
+    "revision": "f9a52863d5886b8dc580",
+    "url": "/react-test-onports/static/js/2.02f876d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test-onports/static/js/2.02f876d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cf4e77db0d742c4a0b8",
+    "url": "/react-test-onports/static/js/main.353712b1.chunk.js"
+  },
+  {
+    "revision": "ca4207683299818b698c",
+    "url": "/react-test-onports/static/js/runtime-main.be786b0c.js"
+  },
+  {
+    "revision": "7db5c8243ee36b7b6d54429e7b8cef8e",
+    "url": "/react-test-onports/static/media/history.7db5c824.png"
+  },
+  {
+    "revision": "9869754f9bd43da086b8e2c7c3300f42",
+    "url": "/react-test-onports/static/media/settings.9869754f.png"
+  },
+  {
+    "revision": "762919f342184f7c1591a7f50d79863d",
+    "url": "/react-test-onports/static/media/shop.762919f3.png"
+  },
+  {
+    "revision": "7279bc42d394c09a03eb5bc4bb53d73b",
+    "url": "/react-test-onports/static/media/user.7279bc42.png"
+  }
+]);
