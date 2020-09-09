32210e26d5dab2c5e741b07c26ae1fa988040924
aniruddhparwal <aniruddhparwal@gmail.com>
2020-09-09 22:38:31 +0530

    Ui Improved for mobile

--- src/components/ClientList.js
+++ src/components/ClientList.js
@@ -1,23 +1,35 @@
-import React from 'react'
-import ClientTableEntry from './ClientTableEntry'
-import topList from './../data/top10details.json'
+import React from "react";
+import ClientTableEntry from "./ClientTableEntry";
+import topList from "./../data/top10details.json";
 
 function ClientList() {
-    console.log("aaa", topList)
-    return (
-        <div className="clientList">
-            <div className="clientList__heading">Top 10 Clients</div>
-            <hr />
-            <div className="clientList__tableHead">
-                <span>Shop Name</span>
-                <span>Service Status</span>
-                <span>Billing</span>
-                <span>Paid</span>
-            </div>
-            {topList.map((item) => { return (<ClientTableEntry name={item.name} serviceStatus={item.serviceStatus} billing={item.billing} paid={item.Paid} />) })}
-
+  console.log("aaa", topList);
+  return (
+    <div className="clientList">
+      <div className="clientList__titleBar">
+        <div className="clientList__heading">Top 10 Clients</div>
+        <hr />
+        <div className="clientList__tableHead">
+          <span>Shop Name</span>
+          <span>Service Status</span>
+          <span>Billing</span>
+          <span>Paid</span>
         </div>
-    )
+      </div>
+      <div className="clientList__entries">
+        {topList.map((item) => {
+          return (
+            <ClientTableEntry
+              name={item.name}
+              serviceStatus={item.serviceStatus}
+              billing={item.billing}
+              paid={item.Paid}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default ClientList
+export default ClientList;
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,32 +1,61 @@
-import React from 'react'
-import MainCards from './MainCards'
-import ClientList from './ClientList'
-import SideCards from './SideCards'
+import React from "react";
+import MainCards from "./MainCards";
+import ClientList from "./ClientList";
+import SideCards from "./SideCards";
 
 export default function MainContainer() {
-    return (
-        <div className="mainContainer">
-            <span className="titleText">Dashboard</span>
-            <div className="container">
-                <div className="row">
-                    <div className="col"><MainCards title="New Client" value={342} color="blue" /></div>
-                    <div className="col"><MainCards title="Pending Request" value={2700} color="lightGreen" /></div>
-                    <div className="col"><MainCards title="Total Client" value={2343} color="pink" /></div>
-                </div>
-            </div>
-
-            <div className="row mainContainer__bottom">
-                <div className=" col-7 mainContainer__bottomLeft">
-                    <ClientList />
-                </div>
-                <div className=" col mainContainer__bottomRight">
-                    <SideCards value={2343} title="Overall Performance" increaseValue={20} color="pink" />
-                    <SideCards value={2343} title="Active Installation" increaseValue={20} color="lightGreen" />
-                    <SideCards value={2343} title="Total Download" increaseValue={20} color="blue" />
-                    <SideCards value={2343} title="Review/Ratings" increaseValue={20} color="purple" />
-                </div>
-            </div>
+  return (
+    <div className="mainContainer">
+      <span className="titleText">Dashboard</span>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <MainCards title="New Client" value={342} color="blue" />
+          </div>
+          <div className="col">
+            <MainCards
+              title="Pending Request"
+              value={2700}
+              color="lightGreen"
+            />
+          </div>
+          <div className="col">
+            <MainCards title="Total Client" value={2343} color="pink" />
+          </div>
         </div>
+      </div>
 
-    )
+      <div className="row mainContainer__bottom">
+        <div className=" col-md-7 col-sm-12 mainContainer__bottomLeft">
+          <ClientList />
+        </div>
+        <div className=" col mainContainer__bottomRight">
+          <SideCards
+            value={2343}
+            title="Overall Performance"
+            increaseValue={20}
+            color="pink"
+          />
+          <SideCards
+            value={2343}
+            title="Active Installation"
+            increaseValue={20}
+            color="lightGreen"
+          />
+          <SideCards
+            value={2343}
+            title="Total Download"
+            increaseValue={20}
+            color="blue"
+          />
+          <SideCards
+            value={2343}
+            title="Review/Ratings"
+            increaseValue={20}
+            color="purple"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
